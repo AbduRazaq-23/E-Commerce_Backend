8d1b03e3890fa6d818b87231e0a924729288228a
Abdurazaq333 <irazaqr986@gmail.com>
2024-08-22 12:43:40 +0500

    db connect

---
+++ src/db/dbConnect.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("DB connected");
+  } catch (error) {
+    console.error("error while connecting db : ", error);
+  }
+};
+
+export { connectDB };
