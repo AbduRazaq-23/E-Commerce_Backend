08cdd54d917a7fac2df4499648e59a83de80af5c
Abdurazaq333 <irazaqr986@gmail.com>
2024-08-23 16:23:30 +0500

    add user routes

--- src/app.js
+++ src/app.js
@@ -6,5 +6,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(express.static("public"));
+
+//@dec import routes
+import userRoutes from "./routes/user.route.js";
+
+app.use("/api/v1/users", userRoutes);
 
 export default app;
---
+++ src/routes/user.route.js
@@ -0,0 +1,30 @@
+import { Router } from "express";
+import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+import {
+  registerUser,
+  logInUser,
+  logOutUser,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateAvatar,
+} from "../controllers/user.controller.js";
+
+router.route("/register").post(upload.single("avatar"), registerUser);
+router.route("/logIn").post(logInUser);
+
+//@dec secure route
+router.route("/logOut").post(verifyJWT, logOutUser);
+
+router.route("/changePassword").post(verifyJWT, changeCurrentPassword);
+router.route("/getcurrentuser").get(verifyJWT, getCurrentUser);
+router.route("/updatedetails").patch(verifyJWT, updateAccountDetails);
+router
+  .route("/updateavatar")
+  .patch(verifyJWT, upload.single("avatar"), updateAvatar);
+
+export default router;
