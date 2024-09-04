c06ff48f912e034826428ed0a82845574b51770d
Abdurazaq333 <irazaqr986@gmail.com>
2024-09-04 13:07:35 +0500

    add the cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
